83731c9291189f90ce33dd2efdc1781dec81a1de
Ryan Walters <shout@ryanwalters.co>
2015-12-14 00:51:27 -0600

    added token routes

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Config = require('../config/config');
+const Token = require('./controllers/token');
 const User = require('./controllers/user');
 
 
@@ -13,5 +14,10 @@ module.exports = [
     { method: 'POST', path: '/user', config: User.create },
     { method: 'GET', path: '/user/{id}', config: User.find },
     { method: 'PUT', path: '/user/{id}', config: User.update },
-    { method: 'DELETE', path: '/user/{id}', config: User.delete }
+    { method: 'DELETE', path: '/user/{id}', config: User.delete },
+
+    // Tokens
+
+    { method: 'POST', path: '/token/refresh', config: Token.refresh },
+    { method: 'POST', path: '/token/access', config: Token.access }
 ];

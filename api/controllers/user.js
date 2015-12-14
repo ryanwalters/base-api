e91847f4e36addad0acf0abcfc045abd7e7bbc15
Ryan Walters <rwalters@eb.com>
2015-12-14 16:49:19 -0600

    created basic user.create

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const _ = require('lodash');
+const Boom = require('boom');
+const Config = require('../../config/config');
 const Joi = require('joi');
+const Jwt = require('jsonwebtoken');
 const Scopes = require('../../config/constants').Scopes;
+const UserModel = require('../models').User;
 
 module.exports = {
-
 
     // Create user
 
@@ -12,13 +16,38 @@ module.exports = {
         auth: false,
         handler: (request, reply) => {
 
-            return reply('user creation endpoint');
+            // todo: save jti to user
+
+            UserModel.create(_.assign({}, request.payload))
+                .then((user) => {
+
+                    return reply({
+                        accessToken: Jwt.sign({
+                            scope: [`user-${user.id}`]
+                        }, Config.get('/jwt/secret'), {
+                            audience: user.username,
+                            expiresIn: 60 * 60 * 24
+                        })
+                    });
+                })
+                .catch((error) => {
+
+                    error.errors.forEach(error => {
+
+                        switch (error.type) {
+                            case 'unique violation':
+                                return reply(Boom.conflict('User already exists.'));
+                            default:
+                                return reply(Boom.badRequest());
+                        }
+                    });
+                });
         },
         validate: {
             payload: {
-                username: Joi.string().alphanum().min(3).max(30).description('Username must be unique'),
-                email: Joi.string().email().required().description('Valid email required'),
-                password: Joi.string().min(6).description('Password must be at least 6 characters'),
+                username: Joi.string().alphanum().min(3).max(30).required(),
+                email: Joi.string().email().required().required(),
+                password: Joi.string().min(6),
                 display_name: Joi.string().min(3).max(30)
             }
         }

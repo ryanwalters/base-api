bc496a3c468dbe11f52108a8d90abc23e980afb8
Ryan Walters <shout@ryanwalters.co>
2015-12-15 02:22:21 -0600

    stub process of creating refresh token

--- api/controllers/token.js
+++ api/controllers/token.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const Config = require('../../config/config');
+const Jwt = require('jsonwebtoken');
+const Scopes = require('../../config/constants').Scopes;
+const UserModel = require('../models').User;
+
 module.exports = {
 
     // Access token
@@ -12,16 +17,32 @@ module.exports = {
     },
 
 
-    // Refresh token
+    // Generate refresh token when user logs in
 
     refresh: {
-        auth: {
-            strategy: 'jwt-refresh',
-            scope: false
-        },
+        auth: false,
         handler: (request, reply) => {
 
-            return reply('refresh token');
+            /**
+             * Steps:
+             * 1. lookup email
+             * 2. if exists, compare password
+             * 3. if valid, generate jti and update user
+             * 4. return signed jwt
+             */
+
+            // todo: add facebook, gmail, linkedin strategies; 2. if isAuthenticated, continue to 3
+
+            return reply('generate refresh token');
+
+            /*return reply({
+                accessToken: Jwt.sign({
+                    jti: user.jti,
+                    scope: [`${Scopes.USER}-${user.id}`]
+                }, Config.get('/jwt/secret'), {
+                    audience: user.username
+                })
+            });*/
         }
     }
 };

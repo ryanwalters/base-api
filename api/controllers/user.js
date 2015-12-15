ec433f74d5a3eefe3de3dc78f6f68e0c0c9421ee
Ryan Walters <rwalters@eb.com>
2015-12-15 16:38:59 -0600

    make password required (until implementing social login)

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -35,7 +35,7 @@ module.exports = {
             payload: {
                 username: Joi.string().alphanum().min(3).max(30).required(),
                 email: Joi.string().email().required(),
-                password: Joi.string().min(6),
+                password: Joi.string().min(6).required(),
                 display_name: Joi.string().min(3).max(30)
             }
         }
